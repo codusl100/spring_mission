bd658c1fec4008b070abc63aa1a43c8cbbd24a55
codus <codusl100@naver.com>
2023-11-27 22:47:36 +0900

    :sparkles: FEAT: 회원가입(간단히) API 구현

--- src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
@@ -24,8 +24,10 @@ public enum ErrorStatus implements BaseErrorCode {
     // 예시,,,
     ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다."),
     // For test
-    TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "이거는 테스트");
+    TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "이거는 테스트"),
 
+    // 음식 종류
+    FOOD_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "FOODTYPE4001", "음식 종류를 찾을 수 없습니다.");
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
